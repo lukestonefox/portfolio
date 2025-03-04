e471564dec9af4d94aa7f9200d0bd9733a34c5a7
Luke Fox <foxlc@mail.uc.edu>
2025-03-04 16:28:31 -0500

    analytics update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import App from './pages/App'
 import ArchivePage from './pages/ArchivePage'
@@ -8,6 +9,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <Analytics />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/archive" element={<ArchivePage />} />
