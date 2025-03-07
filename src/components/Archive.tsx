693a79497113bd393b460a4c24ec5201b871a706
Luke Fox <foxlc@mail.uc.edu>
2025-03-07 02:10:23 -0500

    prelim

--- src/components/Archive.tsx
+++ src/components/Archive.tsx
@@ -6,33 +6,33 @@ import Chip from "./Chip"
 function Archive() {
     return (
         <>
-            <div className="flex items-center justify-center w-screen h-screen bg-gray-200">
-                <div className="flex flex-col w-3/4 gap-y-2">
+            <div className="flex items-center justify-center w-screen h-screen">
+                <div className="flex flex-col w-full px-32">
                     <div className="flex flex-row items-center mb-8 gap-x-2">
                         <Link to="/">
                             <CornerLeftUp className="duration-200 hover:text-gray-300" />
                         </Link>
                         <h1 className="text-4xl font-semibold">Archive</h1>
                     </div>
-                    <div className="flex flex-row items-center w-full px-4 py-2">
-                        <p className="font-semibold w-[5%]">Year</p>
-                        <p className="font-semibold w-[35%]">Name</p>
-                        <p className="font-semibold w-[20%]">Where</p>
-                        <p className="font-semibold w-[35%]">Technologies</p>
-                        <p className="font-semibold w-[5%]">Links</p>
+                    <div className="grid w-full grid-cols-12 px-4 py-2 font-semibold">
+                        <p className="hidden col-span-1">Year</p>
+                        <p className="col-span-3">Name</p>
+                        <p className="hidden xl:col-span-2">Where</p>
+                        <p className="hidden lg:block lg:col-span-6 xl:col-span-4">Technologies</p>
+                        <p className="hidden xl:col-span-2">Links</p>
                     </div>
                         {ArchiveData.map((project, index) => (
                             <>
-                                <div key={index} className="flex flex-row items-center w-full px-4 py-2">
-                                <p className="w-[5%]">{project.year}</p>
-                                <p className="w-[35%]">{project.name}</p>
-                                <p className="w-[20%]">{project.where}</p>
-                                <div className="flex flex-row gap-2 w-[35%] flex-wrap text-xs">
+                                <div key={index} className="grid w-full grid-cols-12 px-4 py-4">
+                                <p className="hidden col-span-1">{project.year}</p>
+                                <p className="col-span-3">{project.name}</p>
+                                <p className="hidden col-span-2">{project.where}</p>
+                                <div className="flex-row flex-wrap hidden gap-2 text-xs lg:col-span-6 xl:col-span-4 lg:flex">
                                     {project.technologies.map((technology, index) => (
                                         <Chip key={index} label={technology} />
                                     ))}
                                 </div>
-                                {project.github && <a target="_blank" className="duration-200 hover:text-gray-600" href={project.github} title="GitHub"><Github /></a>}
+                                {project.github && <a target="_blank" className="hidden col-span-2 duration-200 hover:text-gray-600" href={project.github} title="GitHub"><Github /></a>}
                             </div>
                             {index !== ArchiveData.length-1 && <hr />}
                             </>
